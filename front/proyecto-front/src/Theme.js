987c7de0439c7bca66b13814cf33a1100f96089f
Erwin Lima <ealiman@correo.url.edu.gt>
2025-03-30 17:15:42 -0600

    Recommendation form
    Se aplican inputs y estilos para su funcion

---
+++ front/proyecto-front/src/Theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#408358",
+        },
+        secondary: {
+            main: "#299C52",
+        }
+    },
+    typography: {
+        fontFamily: "Poppins, sans-serif",
+    },
+});
+
+export default theme;
--- front/proyecto-front/src/pages/RecommendationForm.js
+++ front/proyecto-front/src/pages/RecommendationForm.js
@@ -1,14 +1,73 @@
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import Navbar from "../components/common/Navbar/Navbar";
+import styles from './styles/RecommendationForm.module.css';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import MyTextField from "../components/common/input/MyTextField";
 
 const RecommendationForm = () => {
-    return (
-      <div>
-        <Navbar />
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.RecommendationForm_Body}>
         <h1>Solicitar recomendación</h1>
-        <p>Solicitar</p>
-        {/* Add your login form here */}
+        <div className={styles.RecommendationForm_Container}>
+          <div className={styles.RecommendationForm_Container_Form}>
+            <FormControl>
+              <InputLabel id="select-label-genres">Géneros</InputLabel>
+              <Select
+                labelId="select-label-genres"
+                id="select-genres"
+                label="Géneros"
+                onChange={(e) => console.log(e.target.value)}
+              >
+                <MenuItem value={'SA'}>Salsa</MenuItem>
+                <MenuItem value={'RE'}>Reggeaton</MenuItem>
+                <MenuItem value={'RO'}>Rock</MenuItem>
+                <MenuItem value={'PO'}>Pop</MenuItem>
+              </Select>
+
+              <InputLabel id="select-label-country">País</InputLabel>
+              <Select
+                labelId="select-label-country"
+                id="select-country"
+                label="País"
+                onChange={(e) => console.log(e.target.value)}
+              >
+                <MenuItem value={'PE'}>Perú</MenuItem>
+                <MenuItem value={'CO'}>Colombia</MenuItem>
+                <MenuItem value={'AR'}>Argentina</MenuItem>
+                <MenuItem value={'CL'}>Chile</MenuItem>
+              </Select>
+
+              <InputLabel id="input-label-quantity">Cantidad de recomendaciones</InputLabel>
+              <MyTextField
+                labelText="Cantidad de recomendaciones"
+                type="number"
+              />
+
+              <Button
+                component="label"
+                role={undefined}
+                variant="outlined"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+              >
+                Subir fotografía
+              </Button>
+
+              <Button 
+                variant="contained" 
+                color="secondary"
+                sx={{ width: '468px', height: '65px'}}
+                >
+                Obtener recomendación</Button>
+
+            </FormControl>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default RecommendationForm;
